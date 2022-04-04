b7157c15f00b176a19ed0710639b0b35ea025634
reptinias <k.spar@hotmail.com>
2022-04-04 11:29:06 +0200

    First push af mini-project
    En kNN pokedex og en a-star path finding

---
+++ PokeDex/A-star.py
@@ -0,0 +1,128 @@
+class Node():
+    #A node class for A* Pathfinding algorithm
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0 # Distance between current node and the start node
+        self.h = 0 # Distance between current node and the end node. The heuristic function
+        self.f = 0 # Total cost function. This we want to minimize
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+def astar(maze, start, end):
+    # This returns a list, containing the path from start to end as tuples
+
+    # Create start and end node
+    start_node = Node(None, start) # Start node with start as the position and no parent
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end) # End node with end as position and no parent
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both frontier and closed list
+    frontier = [] # Nodes that is potential part of the path
+    closed_list = [] # Nodes that have been looked upon
+
+    # Add the start node
+    frontier.append(start_node)
+
+    # Loop until you find the end. i.e when there are no more nodes in frontier
+    while len(frontier) > 0:
+        # Get the current node
+        current_node = frontier[0] # First node in the frontier list
+        current_index = 0 # Set the index to the index of the first item in a list
+
+        # Loop through the frontier list
+        for index, item in enumerate(frontier):
+            # If the total cost of the current looked upon node, is higher than a node in the frontier
+            # then set the current looked upon node to the node with a lower total cost
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off frontier, add to closed list
+        frontier.pop(current_index)
+        closed_list.append(current_node)
+
+        # If the current node is equal to end node, then the goal have been found
+        if current_node == end_node:
+            path = [] # This is the list that contains the path from end point to start. So this needs to be reversed
+            current = current_node
+            while current is not None:
+                path.append(current.position) # Append the current node position to the path list
+                current = current.parent # Now set the current node, to the parent of the previous appended node
+            return path[::-1]  # Return the reversed path
+
+        # Generate children
+        children = [] # This list will contain the children for this iteration of the while loop
+        for neighbors in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:  # Adjacent squares
+
+            # Get the position of the neighbor node for this iteration of the for loop
+            # Take the x position of the current node and add it to the current iteration of the new_position x position
+            # Take the y position of the current node and add it to the current iteration of the new_position y position
+            neighbor_position = (current_node.position[0] + neighbors[0], current_node.position[1] + neighbors[1])
+
+            # Check whether the neighbor node is inside the maze
+            if neighbor_position[0] > (len(maze) - 1) or neighbor_position[0] < 0 or neighbor_position[1] > (
+                    len(maze[len(maze) - 1]) - 1) or neighbor_position[1] < 0:
+                continue # Skip this iteration of the for loop
+
+            # Check if the neighbor node is walkable
+            if maze[neighbor_position[0]][neighbor_position[1]] != 0:
+                continue # Skip this iteration of the for loop
+
+            # Create new node with the position of the neighbor and the parent are the current looked upon note
+            new_node = Node(current_node, neighbor_position)
+
+            # Append the neighbor node to the children list
+            children.append(new_node)
+
+        # Loop through all children
+        for child in children:
+
+            # If the child is in the closed list, then just skip this iteration of the for loop
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Calculate the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + (
+                        (child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # If the child is in the frontier list, and the child is further in the maze, the skip the iteration
+            for open_node in frontier:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            frontier.append(child)
+
+
+
+def main():
+    # Create a maze
+    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    # Create start and end point
+    start = (0, 0)
+    end = (9, 9)
+
+    path = astar(maze, start, end)
+    print(path)
+
+if __name__ == '__main__':
+    main()
+
---
+++ PokeDex/main.py
@@ -0,0 +1,75 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import cv2
+import os
+from imutils import paths
+
+# resize the image to a fixed size to 32 x 32 pixels, then flatten the image into
+# a list of raw pixel intensities
+# output feature vector will be 32 x 32 x 3 = 3072
+def image_to_feature_vector(image, size=(32, 32)):
+	return cv2.resize(image, size).flatten()
+
+def extract_color_histogram(image, bins=(8, 8, 8)):
+	# extract a 3D color histogram from the HSV color space using
+	# the supplied number of `bins` per channel
+	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
+		[0, 180, 0, 256, 0, 256])
+	# normalizing the histogram
+	cv2.normalize(hist, hist)
+	# return the flattened histogram as the feature vector that is 8 x 8 x 8 = 512
+	return hist.flatten()
+
+# grab the list of images that we'll be describing
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images("dataset"))
+
+# initialize the raw pixel intensities matrix, the features matrix and labels list
+rawImages = []
+features = []
+labels = []
+
+# loop over the input images
+for (i, imagePath) in enumerate(imagePaths):
+	# load the image and extract the class label (assuming that our
+	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+	image = cv2.imread(imagePath)
+	label = imagePath.split(os.path.sep)[-1].split(".")[0]
+	# extract raw pixel intensity "features", followed by a color
+	# histogram to characterize the color distribution of the pixels in the image
+	pixels = image_to_feature_vector(image)
+	hist = extract_color_histogram(image)
+	# update the raw images, features, and labels matricies respectively
+	rawImages.append(pixels)
+	features.append(hist)
+	labels.append(label)
+	# show an update every 10 images
+	if i > 0 and i % 10 == 0:
+		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
+
+# partition the data into training and testing splits, using 75% of the data for training and the remaining 25% for testing
+(trainRI, testRI, trainRL, testRL) = train_test_split(rawImages, labels, test_size=0.25, random_state=42)
+(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(features, labels, test_size=0.25, random_state=42)
+
+
+# train and evaluate a k-NN classifer on the raw pixel intensities
+print("[INFO] evaluating raw pixel accuracy...")
+model = KNeighborsClassifier(n_neighbors=5,
+	n_jobs=-1)
+
+model.fit(trainRI, trainRL)
+# findes the mean accuracy of the test data and label
+acc = model.score(testRI, testRL)
+print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
+
+
+# train and evaluate a k-NN classifer on the histogram
+# representations
+print("[INFO] evaluating histogram accuracy...")
+model = KNeighborsClassifier(n_neighbors=5,
+	n_jobs=-1)
+model.fit(trainFeat, trainLabels)
+acc = model.score(testFeat, testLabels)
+print("[INFO] histogram accuracy: {:.2f}%".format(acc * 100))
+
